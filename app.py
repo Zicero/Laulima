d520d506600abbb75101d4287d7072a781089acd
Tyler Chong <tylrchong@gmail.com>
2017-03-24 20:32:19 -1000

    Templates

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup
 from lxml.html.clean import clean_html
 from jinja2 import Template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    template = Template('Hello {{ name }}')
+    return render_template('pages/index.html')
 
-    return template.render(name = 'Lol')
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 f = open('test.txt', 'r')
 
@@ -24,4 +28,3 @@ html = f.read()
 soup = BeautifulSoup(clean_html(html), 'lxml')
 
 print soup.find_all('li', 'nav-menu')
-
